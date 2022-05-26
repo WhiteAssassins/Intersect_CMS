63d4cdf033c0eb93ffdd4a71d4db21324e0e2dc7
WhiteAssassins <christopherdavidalbertoroque@gmail.com>
2022-05-26 00:09:30 -0400

    Deletes2

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://127.0.0.1/intersect';
+$config['base_url'] = 'http://127.0.0.1/Intesect_CMS';
 
 /*
 |--------------------------------------------------------------------------
