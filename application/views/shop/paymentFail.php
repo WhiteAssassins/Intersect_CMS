dc1fe73952ef1693d12a22a4fe56ddb5f10f51b1
WhiteAssassins <christopherdavidalbertoroque@gmail.com>
2022-05-26 00:25:08 -0400

    Update paymentFail.php

--- application/views/shop/paymentFail.php
+++ application/views/shop/paymentFail.php
@@ -1,16 +1,13 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Transaction Fail - Codeigniter Paypal Integration Example - CodingsPoint.com</title>
-  <!-- Latest CSS -->
- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
- <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script> 
+  <title>Transaccion Fallida</title>
 </head>
 <body>
   <div class="container">
-    <h2 class="mt-3 mb-3">Transaction Detalis</h2>
+    <h2 class="mt-3 mb-3">Detalles de la Transaccion</h2>
     <div class="row">
-       <p>Sorry! Your last transaction was cancelled.</p>
+       <p>Lo Sentimos! Su Ultima Transaccion a sido Cancelada.</p>
     </div>
   </div>
 </body>
