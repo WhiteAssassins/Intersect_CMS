703a61d1f869c91171c2b154d127a7b39547fed9
WhiteAssassins <christopherdavidalbertoroque@gmail.com>
2022-05-26 00:21:21 -0400

    Update news.php

--- application/views/admin/news.php
+++ application/views/admin/news.php
@@ -2,11 +2,6 @@
 $resultadonews = $this->db->get('news');
 $restnews = $resultadonews->result_array();
 ?>
-
-
-
-
-
 <div class="modal fade" id="modal_addnews" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content cards-novo">
@@ -18,25 +13,19 @@ $restnews = $resultadonews->result_array();
         </div>
         <div class="modal-body mx-3">
             <form action="<?php echo base_url('admin/addnews'); ?>" method="post"  enctype="multipart/form-data">
-                <div class="md-form mb-5">
-                    
+                <div class="md-form mb-5">                    
                     <input type="text" id="defaultForm-email" class="form-control validate" name="title">
                     <label for="defaultForm-email">Titulo</label>
                 </div>
-
-                <div class="md-form mb-4">
-                   
+                <div class="md-form mb-4">                  
                     <input type="text" id="defaultForm-pass" class="form-control validate" name="descrip">
                     <label for="defaultForm-pass">Descripción</label>
                 </div>
-                <div class="md-form mb-4 pink-textarea active-pink-textarea">
-  
+                <div class="md-form mb-4 pink-textarea active-pink-textarea"> 
                 <textarea id="tiny" name="txt" placeholder="Texto de la Noticia"></textarea>
-</div>
-                
+</div>              
         </div>
         <div class="md-form mb-4">
-
 <div class="file-field">
     <a class="btn-file-c btn-floating blue-gradient mt-0 float-left">
         <i class="fas fa-paperclip" aria-hidden="true"></i>
@@ -54,49 +43,22 @@ $restnews = $resultadonews->result_array();
             </div>
         </div>
     </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
 <div class="container my-5 py-5">
-
 <div class="card card-cascade narrower cards-novo">
-
-  <!--Card image-->
   <div
     class="view view-cascade gradient-card-header blue-gradient narrower py-2 mx-4 mb-3 d-flex justify-content-between align-items-center">
-
     <div>
           <button type="button" class="btn btn-outline-white btn-rounded btn-sm px-2 waves-effect waves-light btn_modal_addnews " >
             <i class="fas fa-plus mt-0"></i>
           </button>
           <a href="#" style="padding-left: 400px;"  class="white-text mx-3">Noticias</a>
         </div>
-        
-
-    
   </div>
-  <!--/Card image-->
-
   <div class="px-4 ">
-
     <div class="table-wrapper">
-      <!--Table-->
       <table class="table table-hover mb-0">
-
-        <!--Table head-->
         <thead>
           <tr>
-          
             <th class="th-lg">
               <a>Titulo
                 <i class="fas fa-sort ml-1"></i>
@@ -119,15 +81,12 @@ $restnews = $resultadonews->result_array();
             </th>
           </tr>
         </thead>
-        <!--Table head-->
-
-        <!--Table body-->
         <tbody>
-        <?php 
-                                     
-                                      foreach (array_reverse($restnews) as $key) { 
-                                        
-                                  ?>
+
+        <?php                              
+          foreach (array_reverse($restnews) as $key) {                                        
+        ?>
+
           <tr>
             <td><?php echo $key['title']; ?></td>
             <td><?php echo $key['descrip']; ?></td>
@@ -142,8 +101,8 @@ $restnews = $resultadonews->result_array();
                 </button>
                 </label>
                 </form>
-                         
-               
+
+
                 <form method="POST" action="<?php echo base_url('admin/editnews'); ?>" style="padding-left: 10px;">
                 <label class="badge badge-warning">
                     <input type="hidden" name="id" value="<?php echo $key['id']; ?>">
@@ -161,12 +120,8 @@ $restnews = $resultadonews->result_array();
                             }
                         ?>  
         </tbody>
-        <!--Table body-->
       </table>
-      <!--Table-->
     </div>
-
   </div>
-
 </div>
 </div>
