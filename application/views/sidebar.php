197e12777f5fe00b8ce65282a33ebc384dc92f6c
WhiteAssassins <christopherdavidalbertoroque@gmail.com>
2022-05-27 02:10:53 -0400

    Updates Icons

--- application/views/sidebar.php
+++ application/views/sidebar.php
@@ -14,28 +14,33 @@
     </li>
     <li>
       <ul class="collapsible collapsible-accordion">
-      <li><a href="<?php echo base_url('home');?>" class="collapsible-header waves-effect" tabindex="0"><i class="fas fa-chevron-right"></i> Inicio</a>
+      <li><a href="<?php echo base_url('home');?>" class="collapsible-header waves-effect" tabindex="0"><i class="fas fa-home"></i> Inicio</a>
         </li>
-        <li><a href="<?php echo base_url('admin');?>" class="collapsible-header waves-effect" tabindex="0"><i class="fas fa-chevron-right"></i> Dashboard</a>
+        <li><a href="<?php echo base_url('admin');?>" class="collapsible-header waves-effect" tabindex="0"><i class="fas fa-tachometer-alt"></i> Dashboard</a>
         </li>
-        <li><a href="<?php echo base_url('admin/shop');?>" class="collapsible-header waves-effect" tabindex="0"><i class="fas fa-chevron-right"></i> Tienda</a>
+        <li><a href="<?php echo base_url('admin/shop');?>" class="collapsible-header waves-effect" tabindex="0"><i class="fas fa-shopping-cart"></i> Tienda</a>
         </li>
-        <li><a href="<?php echo base_url('admin/news');?>" class="collapsible-header waves-effect" tabindex="0"><i class="fas fa-chevron-right"></i> Noticias</a>
+        <li><a href="<?php echo base_url('admin/news');?>" class="collapsible-header waves-effect" tabindex="0"><i class="fas fa-newspaper"></i> Noticias</a>
         </li>
-        <li><a href="<?php echo base_url('admin/objects');?>" class="collapsible-header waves-effect" tabindex="0"><i class="fas fa-chevron-right"></i> Objetos</a>
+        <li><a href="<?php echo base_url('admin/objects');?>" class="collapsible-header waves-effect" tabindex="0"><i class="fas fa-box"></i> Objetos</a>
         </li>
-        <li><a href="<?php echo base_url('admin/maps');?>" class="collapsible-header waves-effect" tabindex="0"><i class="fas fa-chevron-right"></i> Mapas</a>
+        <li><a href="<?php echo base_url('admin/maps');?>" class="collapsible-header waves-effect" tabindex="0"><i class="fas fa-map"></i> Mapas</a>
         </li>
-        <li><a href="<?php echo base_url('admin/events');?>" class="collapsible-header waves-effect" tabindex="0"><i class="fas fa-chevron-right"></i> Eventos</a>
+        <li><a href="<?php echo base_url('admin/events');?>" class="collapsible-header waves-effect" tabindex="0"><i class="fas fa-calendar"></i> Eventos</a>
         </li>
-        <li><a href="<?php echo base_url('logs');?>" class="collapsible-header waves-effect" tabindex="0"><i class="fas fa-chevron-right"></i> Logs</a>
+        <li><a href="<?php echo base_url('logs');?>" class="collapsible-header waves-effect" tabindex="0"><i class="fas fa-stream"></i> Logs</a>
         </li>
-        <li><a href="<?php echo base_url('admin/adminaccounts');?>" class="collapsible-header waves-effect" tabindex="0"><i class="fas fa-chevron-right"></i> Cuentas Administrador</a>
+        <li><a href="<?php echo base_url('admin/adminaccounts');?>" class="collapsible-header waves-effect" tabindex="0"><i class="fas fa-user-alt"></i> Cuentas Administrador</a>
         </li>
-        <li><a href="<?php echo base_url('config');?>" class="collapsible-header waves-effect" tabindex="0"><i class="fas fa-chevron-right"></i> Configuración</a>
+        <li><a href="<?php echo base_url('config');?>" class="collapsible-header waves-effect" tabindex="0"><i class="fas fa-cog"></i> Configuración</a>
         </li>
-        <li><a href="<?php echo base_url('home/logout');?>" class="collapsible-header waves-effect" tabindex="0"><i class="fas fa-chevron-right"></i> Desconectarse</a>
+        <li><a href="<?php echo base_url('home/logout');?>" class="collapsible-header waves-effect" tabindex="0"><i class="fas fa-sign"></i> Desconectarse</a>
         </li>
+
+
+        
+
+
       </ul>
     </li>
 </div>
