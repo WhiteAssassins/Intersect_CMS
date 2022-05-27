72b891ca4f9342af707f0daf4a2d47bcd0ff88a8
WhiteAssassins <christopherdavidalbertoroque@gmail.com>
2022-05-26 23:00:26 -0400

    Create Config.php

---
+++ application/controllers/Config.php
@@ -0,0 +1,95 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+require FCPATH.'vendor/autoload.php';
+use GuzzleHttp\Client;
+class Config extends CI_Controller {
+	function __construct() {
+        parent::__construct();
+        $this->load->model('Apigettoken');
+		$this->load->model('Apiserverinfo');
+		$this->load->model('Apiregister');
+		$this->load->model('Apiserverstats');
+		$this->load->model('Apiusers');
+		$this->load->model('Apiplayers');
+    }
+	
+	public function index()
+	{	
+		if($this->session->userdata('login') == true AND $this->session->userdata('rol') == 1){
+			$this->load->view('header');
+			$this->load->view('sidebar');  
+			$this->load->view('admin/config');
+			$this->load->view('footer');	
+		}else{
+			$base_url = base_url();
+			header("Location: $base_url");
+		}
+
+	}
+
+	public function editcolors(){
+		if($this->session->userdata('login') == true AND $this->session->userdata('rol') == 1){
+		$id = 1;
+		$color1 = $this->input->post('color1');
+		$color2 = $this->input->post('color2');
+		$where = [
+			'id'=>$id,
+
+		];
+		$this->db->where($where);
+		$resultado = $this->db->get('config');
+		$datos = [
+			'color1'=>$color1,
+			'color2'=>$color2,
+
+		];
+		$this->db->where('id', $id);
+        $this->db->update('config', $datos); 
+		$base_url = base_url();
+		header("Location: $base_url/config");
+	}else{
+		$base_url = base_url();
+		header("Location: $base_url");
+	}
+	}
+	
+
+
+	public function mantact(){
+        $conf = $this->db->get('config');
+		$conf1 = $conf->result_array();
+        $datos = array (
+            'mant' => 1,
+           );
+          $this->db->update('config', $datos);
+          $base_url = base_url();
+        header("Location: $base_url/config");
+    }
+
+
+	public function mantdes(){
+        $conf = $this->db->get('config');
+		$conf1 = $conf->result_array();
+        $datos = array (
+            'mant' => 0,
+           );
+          $this->db->update('config', $datos);
+          $base_url = base_url();
+        header("Location: $base_url/config");
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
