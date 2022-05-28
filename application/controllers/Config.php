b548337ecb11078b892ede555ba057bbe57cbd86
WhiteAssassins <christopherdavidalbertoroque@gmail.com>
2022-05-28 01:21:59 -0400

    Privacity

--- application/controllers/Config.php
+++ application/controllers/Config.php
@@ -131,6 +131,110 @@ class Config extends CI_Controller {
 
 
 
+	public function legal(){
+		if($this->session->userdata('login') == true AND $this->session->userdata('rol') == 1){
+		$this->load->view('header');
+        $this->load->view('admin/legal');
+		$this->load->view('footer');
+	}else{
+		$base_url = base_url();
+		header("Location: $base_url");
+	}
+	}
+
+	public function terms(){
+		if($this->session->userdata('login') == true AND $this->session->userdata('rol') == 1){
+		$this->load->view('header');
+        $this->load->view('admin/terms');
+		$this->load->view('footer');
+	}else{
+		$base_url = base_url();
+		header("Location: $base_url");
+	}
+	}
+	public function privacity(){
+		if($this->session->userdata('login') == true AND $this->session->userdata('rol') == 1){
+		$this->load->view('header');
+        $this->load->view('admin/privacity');
+		$this->load->view('footer');
+	}else{
+		$base_url = base_url();
+		header("Location: $base_url");
+	}
+	}
+
+
+	public function changeprivacity(){
+		if($this->session->userdata('login') == true AND $this->session->userdata('rol') == 1){
+		$id = 1;
+		$privacity = $this->input->post('privacity');
+		$where = [
+			'id'=>$id,
+
+		];
+		$this->db->where($where);
+		$resultado = $this->db->get('config');
+		$datos = [
+			'privacity'=>$privacity
+
+		];
+		$this->db->where('id', $id);
+        $this->db->update('config', $datos); 
+		$base_url = base_url();
+		header("Location: $base_url/config");
+	}else{
+		$base_url = base_url();
+		header("Location: $base_url");
+	}
+	}
+	public function changeterms(){
+		if($this->session->userdata('login') == true AND $this->session->userdata('rol') == 1){
+		$id = 1;
+		$terms = $this->input->post('terms');
+		$where = [
+			'id'=>$id,
+
+		];
+		$this->db->where($where);
+		$resultado = $this->db->get('config');
+		$datos = [
+			'terms'=>$terms
+
+		];
+		$this->db->where('id', $id);
+        $this->db->update('config', $datos); 
+		$base_url = base_url();
+		header("Location: $base_url/config");
+	}else{
+		$base_url = base_url();
+		header("Location: $base_url");
+	}
+	}
+
+	public function changelegal(){
+		if($this->session->userdata('login') == true AND $this->session->userdata('rol') == 1){
+		$id = 1;
+		$legal = $this->input->post('legal');
+		$where = [
+			'id'=>$id,
+
+		];
+		$this->db->where($where);
+		$resultado = $this->db->get('config');
+		$datos = [
+			'legal'=>$legal
+
+		];
+		$this->db->where('id', $id);
+        $this->db->update('config', $datos); 
+		$base_url = base_url();
+		header("Location: $base_url/config");
+	}else{
+		$base_url = base_url();
+		header("Location: $base_url");
+	}
+	}
+
 
 
 
