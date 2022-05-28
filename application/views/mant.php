825e1e330d58f9f97dcab3970cb1cb299d4e072e
WhiteAssassins <christopherdavidalbertoroque@gmail.com>
2022-05-28 01:20:55 -0400

    Indentation Mant

--- application/views/mant.php
+++ application/views/mant.php
@@ -1,17 +1,9 @@
 <div class="container" style="padding-top: 100px;">
 <div class="jumbotron text-center cards-novo">
-
-  <!-- Title -->
   <h2 class="card-title h2">Mantemiento</h2>
-  <!-- Subtitle -->
   <p class=" my-4 font-weight-bold">Buenas estamos en Mantenimiento. Pronto Volveremos</p>
-
-  <!-- Grid row -->
   <div class="row d-flex justify-content-center">
-
-    <!-- Grid column -->
     <div class="col-xl-7 pb-2">
-
     <form method="POST" action="<?php echo base_url('mant/login'); ?>" id="form_ban" class="form-admin">
                         <h4 class="text-center font-weight-bold mb-4 pb-2">Entrar como Admin</h4>
                         <div class="md-form mb-4">
@@ -24,17 +16,9 @@
                         </div>
                         <button type="submit" class="btn btn-outline-primary waves-effect">Entrar</button>
                         </form>
-
     </div>
-    <!-- Grid column -->
-
   </div>
-  <!-- Grid row -->
-
-
-
   <div class="pt-2">
   </div>
   </div>
 </div>
-<!-- Jumbotron -->
