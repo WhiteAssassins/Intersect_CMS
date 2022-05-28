c1435e93bf5be4201db606b348e339b563a85cac
WhiteAssassins <christopherdavidalbertoroque@gmail.com>
2022-05-28 01:21:48 -0400

    Terms

---
+++ application/controllers/Terms.php
@@ -0,0 +1,32 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+require FCPATH.'vendor/autoload.php';
+use GuzzleHttp\Client;
+class Terms extends CI_Controller {
+	function __construct() {
+        parent::__construct();
+        $this->load->model('Apigettoken');
+		$this->load->model('Apiserverinfo');
+		$this->load->model('Apiregister');
+		$this->load->model('Apiserverstats');
+		$this->load->model('Apiusers');
+		$this->load->model('Apiplayers');
+    }
+	
+	public function index()
+	{	
+        $conf = $this->db->get('config');
+		$conf1 = $conf->result_array(); 
+       if($conf1['0']['mant'] == 1 AND $this->session->userdata('login') == false){
+            $this->load->view('header');
+            $this->load->view('mant');
+        }else{
+            $this->load->view('header');
+            $this->load->view('navbar');
+            $this->load->view('terms');
+            $this->load->view('footer');
+        } 
+       
+	}
+	
+}
---
+++ application/views/terms.php
@@ -0,0 +1,16 @@
+<?php 
+    $conf = $this->db->get('config');
+    $conf1 = $conf->result_array();
+?>
+<div class="container" style="padding-top: 100px;">
+<div class="jumbotron text-center cards-novo">
+  <h2 class="card-title h2">Terminos y Condiciones</h2>
+  <p class=" my-4 font-weight-bold"><?php echo $conf1[0]['terms']; ?></p>
+  <div class="row d-flex justify-content-center">
+    <div class="col-xl-7 pb-2">
+    </div>
+  </div>
+  <div class="pt-2">
+  </div>
+  </div>
+</div>
