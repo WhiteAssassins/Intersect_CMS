a48b901830d9aada7ad351c6d191034010f94f8a
Christopher David Alberto Roque <christopherdavidalbertoroque@gmail.com>
2022-05-29 22:50:55 -0400

    Update Apimap.php

--- application/models/Apimap.php
+++ application/models/Apimap.php
@@ -8,7 +8,7 @@ class Apimap extends CI_Model{
         $accesstoken = $this->Apigettoken->apitoken();
         $apiip = $this->config->item('apiip');;
         $client = new Client([
-          'base_uri' => 'http://'.$apiip.'/api/v1/gameobjects/map',
+          'base_uri' => 'http://'.$apiip.'/api/v1/gameobjects/map?pageSize=5000',
           'timeout'  => 5.0,
         ]);
         $res = $client->request('POST','',[
