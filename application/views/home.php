f31bab92b750c10792effdf1701a814e01273aaf
Christopher David Alberto Roque <christopherdavidalbertoroque@gmail.com>
2022-05-29 22:50:36 -0400

    Update home.php

--- application/views/home.php
+++ application/views/home.php
@@ -1,4 +1,7 @@
-
+<?php
+    $conf = $this->db->get('config');
+    $conf1 = $conf->result_array();
+?>
 <div class="container my-5 z-depth-1 px-0 rounded">
 <section class="white-text p-4 rounded cards-novo">
   
@@ -66,31 +69,31 @@
   <section class="text-center white-text">
 
     <h2 class="font-weight-bold mb-4 pb-2 text-uppercase">Caracteristicas</h2>
-    <p class="lead mx-auto mb-5">Contamos con Muchas Caracteristicas Atractivas para los Usuarios.</p>
+    <p class="lead mx-auto mb-5"><?php echo $conf1[0]['menuheader']; ?></p>
 
     <div class="row">
 
       <div class="col-md-4 mb-4">
 
-        <i class="fas fa-trophy fa-3x purple-text"></i>
-        <h5 class="font-weight-bold my-4 text-uppercase">PVP</h5>
-        <p class="mb-md-0 mb-5">Sistema PVP Con muchas Recompensas para los mejores guerreros del Reino.
+        <i class="<?php echo $conf1[0]['menu1icon']; ?> fa-3x purple-text"></i>
+        <h5 class="font-weight-bold my-4 text-uppercase"><?php echo $conf1[0]['menu1header']; ?></h5>
+        <p class="mb-md-0 mb-5"><?php echo $conf1[0]['menu1text']; ?>
         </p>
 
       </div>
       <div class="col-md-4 mb-4">
 
-        <i class="fas fa-dog fa-3x purple-text"></i>
-        <h5 class="font-weight-bold my-4 text-uppercase">Mascotas</h5>
-        <p class="mb-md-0 mb-5">Sistema de Mascotas para PVP y PVE. Estas listo para tener un gato un perro o hasta un dragon porque no?.
+        <i class="<?php echo $conf1[0]['menu2icon']; ?> fa-3x purple-text"></i>
+        <h5 class="font-weight-bold my-4 text-uppercase"><?php echo $conf1[0]['menu2header']; ?></h5>
+        <p class="mb-md-0 mb-5"><?php echo $conf1[0]['menu2text']; ?>
         </p>
 
       </div>
       <div class="col-md-4 mb-4">
 
-        <i class="fas fa-hammer fa-3x purple-text"></i>
-        <h5 class="font-weight-bold my-4 text-uppercase">Craftea</h5>
-        <p class="mb-0">Crea desde una pocion, armadura hasta una caja de bombones para esa enamorada que tienes por ahi 👌.
+        <i class="<?php echo $conf1[0]['menu3icon']; ?> fa-3x purple-text"></i>
+        <h5 class="font-weight-bold my-4 text-uppercase"><?php echo $conf1[0]['menu3header']; ?></h5>
+        <p class="mb-0"><?php echo $conf1[0]['menu3text']; ?>
         </p>
 
       </div>
