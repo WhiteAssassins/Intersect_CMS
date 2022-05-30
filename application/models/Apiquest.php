cfda50e27268a7d431bdeafcd4ba0ea061411b09
Christopher David Alberto Roque <christopherdavidalbertoroque@gmail.com>
2022-05-29 22:50:44 -0400

    Update Apiquest.php

--- application/models/Apiquest.php
+++ application/models/Apiquest.php
@@ -8,7 +8,7 @@ class Apiquest extends CI_Model{
         $accesstoken = $this->Apigettoken->apitoken();
         $apiip = $this->config->item('apiip');;
         $client = new Client([
-          'base_uri' => 'http://'.$apiip.'/api/v1/gameobjects/quest',
+          'base_uri' => 'http://'.$apiip.'/api/v1/gameobjects/quest?pageSize=5000',
           'timeout'  => 5.0,
         ]);
         $res = $client->request('POST','',[
