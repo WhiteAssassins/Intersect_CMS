cb6eb379478c8894316b38b553855f10ea7a542c
Christopher David Alberto Roque <christopherdavidalbertoroque@gmail.com>
2022-05-29 22:51:01 -0400

    Update Config.php

--- application/controllers/Config.php
+++ application/controllers/Config.php
@@ -162,7 +162,66 @@ class Config extends CI_Controller {
 		header("Location: $base_url");
 	}
 	}
+	public function menus(){
+		if($this->session->userdata('login') == true AND $this->session->userdata('rol') == 1){
+		$this->load->view('header');
+        $this->load->view('admin/menus');
+		$this->load->view('footer');
+	}else{
+		$base_url = base_url();
+		header("Location: $base_url");
+	}
+	}
 
+
+	public function editmenus(){
+		if($this->session->userdata('login') == true AND $this->session->userdata('rol') == 1){
+		$menuheader = $this->input->post('menuheader');
+
+		$menu1icon = $this->input->post('menu1icon');
+		$menu1header = $this->input->post('menu1header');
+		$menu1text = $this->input->post('menu1text');
+
+		$menu2icon = $this->input->post('menu2icon');
+		$menu2header = $this->input->post('menu2header');
+		$menu2text = $this->input->post('menu2text');
+
+		$menu3icon = $this->input->post('menu3icon');
+		$menu3header = $this->input->post('menu3header');
+		$menu3text = $this->input->post('menu3text');
+
+		$id = 1;
+		$where = [
+			'id'=>$id,
+
+		];
+		$this->db->where($where);
+		$resultado = $this->db->get('config');
+		$datos = [
+			'menuheader'=>$menuheader,
+
+			'menu1icon'=>$menu1icon,
+			'menu1header'=>$menu1header,
+			'menu1text'=>$menu1text,
+
+			'menu2icon'=>$menu2icon,
+			'menu2header'=>$menu2header,
+			'menu2text'=>$menu2text,
+
+			'menu3icon'=>$menu3icon,
+			'menu3header'=>$menu3header,
+			'menu3text'=>$menu3text,
+
+		];
+		$this->db->where('id', $id);
+        $this->db->update('config', $datos); 
+		$base_url = base_url();
+		header("Location: $base_url/config");
+	}else{
+		$base_url = base_url();
+		header("Location: $base_url");
+	}
+	}
 
 	public function changeprivacity(){
 		if($this->session->userdata('login') == true AND $this->session->userdata('rol') == 1){
