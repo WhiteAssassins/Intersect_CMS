a44815ac33d07fbaf8204e92ec6f3524e98c53fd
Christopher David Alberto Roque <christopherdavidalbertoroque@gmail.com>
2022-05-29 22:50:57 -0400

    Update Apievent.php

--- application/models/Apievent.php
+++ application/models/Apievent.php
@@ -8,7 +8,7 @@ class Apievent extends CI_Model{
         $accesstoken = $this->Apigettoken->apitoken();
         $apiip = $this->config->item('apiip');;
         $client = new Client([
-          'base_uri' => 'http://'.$apiip.'/api/v1/gameobjects/event',
+          'base_uri' => 'http://'.$apiip.'/api/v1/gameobjects/event?pageSize=5000',
           'timeout'  => 5.0,
         ]);
         $res = $client->request('POST','',[
