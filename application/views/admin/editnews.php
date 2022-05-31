6abd79d5258a5fc8cd1fac89bf6f656ae88660a3
Christopher David Alberto Roque <christopherdavidalbertoroque@gmail.com>
2022-05-31 18:12:52 -0400

    Update editnews.php

--- application/views/admin/editnews.php
+++ application/views/admin/editnews.php
@@ -9,15 +9,15 @@
         <div class="card  cards-novo">
           <div class="card-body">
             <form class="text-center" style="color: #757575;" action="<?php echo base_url('admin/editnewss'); ?>" method="POST">
-              <h3 class="font-weight-bold my-4 pb-2 text-center ">Editar Noticia</h3>
+              <h3 class="font-weight-bold my-4 pb-2 text-center ">{news}</h3>
                 <input type="hidden" name="id" value="<?php echo $key['id']; ?>">
-              <input type="text" class="form-control mb-4" placeholder="Titulo" value="<?php echo $key['title']; ?>" name="title">
-              <input type="text"class="form-control" placeholder="Descripción" value="<?php echo $key['descrip']; ?>" name="descrip">
+              <input type="text" class="form-control mb-4" placeholder="{title}" value="<?php echo $key['title']; ?>" name="title">
+              <input type="text"class="form-control" placeholder="{description}" value="<?php echo $key['descrip']; ?>" name="descrip">
               <br>
-              <textarea id="tiny" name="txt" placeholder="Texto de la Noticia"><?php echo $key['txt']; ?></textarea>
+              <textarea id="tiny" name="txt" placeholder="{textnews}"><?php echo $key['txt']; ?></textarea>
               <div class="text-center">
-                <button type="submit" class="btn btn-outline-orange btn-rounded my-4 waves-effect">Editar</button>
-                <a href="<?php echo base_url('admin/news'); ?>" type="button" class="btn btn-outline-blue btn-rounded my-4 waves-effect">Volver</a>
+                <button type="submit" class="btn btn-outline-orange btn-rounded my-4 waves-effect">{edit}</button>
+                <a href="<?php echo base_url('admin/news'); ?>" type="button" class="btn btn-outline-blue btn-rounded my-4 waves-effect">{return}</a>
               </div>
             </form>
             <?php } ?>
