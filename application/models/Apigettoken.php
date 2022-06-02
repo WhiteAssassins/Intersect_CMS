2777ed895fe7dfb9482d86bc3b36f577d8afd0e9
Christopher David Alberto Roque <christopherdavidalbertoroque@gmail.com>
2022-06-02 00:18:01 -0400

    Update Apigettoken.php

--- application/models/Apigettoken.php
+++ application/models/Apigettoken.php
@@ -3,11 +3,11 @@ require FCPATH.'vendor/autoload.php';
 use GuzzleHttp\Client;
 class Apigettoken extends CI_Model{
     public function apitoken(){
-    $apiip = $this->config->item('apiip');;
+    $apiip = $this->config->item('apiip');
     $apiuser = [
         'grant_type' => "password",
-       'username' => "WhiteAssassins",
-        'password' => "079DC5B862FE910FBEF8602265613A84AF5A5E5A2DA0D912EECEF637CF8957D1"
+       'username' => $this->config->item('apiuser'),
+        'password' => $this->config->item('apipass'),
           ];
         $client = new Client([
       'base_uri' => 'http://'.$apiip.'/api/oauth/token',
