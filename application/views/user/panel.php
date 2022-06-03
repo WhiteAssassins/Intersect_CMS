e88bc6e26f60d501ddf1b083442d84d928f35760
Christopher David Alberto Roque <christopherdavidalbertoroque@gmail.com>
2022-06-02 21:07:54 -0400

    Update panel.php

--- application/views/user/panel.php
+++ application/views/user/panel.php
@@ -11,7 +11,19 @@
     <div class="col-xl-7 pb-2">
       <br>
     <h4 class="text-center font-weight-bold mb-4 pb-2">Saldo Disponible: <?php echo $rest[0]['balance']; ?><a href="<?php echo base_url('userpanel/recharge'); ?>"><span class="badge badge-success ml-2"><i class="fas fa-plus"></i></span></a></h4>
-    
+            <form method="POST"  action="<?php echo base_url('config/changelang'); ?>"  class="form-admin">
+                <h4 class="text-center font-weight-bold mb-4 pb-2">Cambiar Contraseña</h4>
+                <div class="md-form mb-4">
+                    <input type="password" id="prefixInside" class="form-control" name="password" placeholder="Contraseña" required>
+                </div>
+                <div class="md-form mb-4">
+                    <input type="password" id="prefixInside" class="form-control" name="newpassword" placeholder="Nueva Contraseña" required>
+                </div>
+                <div class="md-form mb-4">
+                    <input type="password" id="prefixInside" class="form-control" name="newpassword1" placeholder="Repetir Nueva Contraseña"  required>
+                </div>
+                <button href="<?php echo base_url('config/lang'); ?>" type="submit" class="btn btn-outline-info waves-effect" style="margin-left: 65px;">{change}</button>
+            </form>
     </div>
   </div>
   <div class="pt-2">
