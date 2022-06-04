e5f4cb2ee30bc99d7d9483fcbbe7cd856d02aa80
Christopher David Alberto Roque <christopherdavidalbertoroque@gmail.com>
2022-06-03 20:48:58 -0400

    Revert "Optimization"
    This reverts commit e0c004137bcb924abf308b8769f18678dda025a1.

--- public/js/cards.js
+++ public/js/cards.js
@@ -1,3 +1,93 @@
-jQuery(function(b){b(document).on("click.card",".card",function(a){var d=b(this).find(".card-reveal");if(d.length){var c=b(a.target);a=c.is(".card-reveal .card-title");var f=c.is(".card-reveal .card-title i"),e=c.is(".card .activator");c=c.is(".card .activator i");a||f?g(d):(e||c)&&d.css({display:"block"}).velocity({translateY:"-100%"},{duration:300,queue:!1,easing:"easeInOutQuad"})}});var g=function(a){a.velocity({translateY:0},{duration:225,queue:!1,easing:"easeInOutQuad",complete:function(){a.css({display:"none"})}})};
-b(".rotate-btn").on("click",function(){b(this).closest(".card").toggleClass("flipped")});b(window).on("load",function(){b(".card-rotating").each(function(){var a=b(this),d=a.parent(),c=a.find(".front"),f=a.find(".back"),e=a.find(".front").outerHeight();a=a.find(".back").outerHeight();e>a?b(d,f).height(e):e<a?b(d,c).height(a):b(d).height(a)})});b(".card-share > a").on("click",function(a){a.preventDefault();b(this).toggleClass("share-expanded").parent().find("div").toggleClass("social-reveal-active")});
-b(".map-card").on("click",function(){b(this).find(".card-body").toggleClass("closed")})});
+jQuery(function ($) {
+
+  $(document).on('click.card', '.card', function (e) {
+
+    const $this = $(this);
+    const $reveal = $this.find('.card-reveal');
+
+    if ($reveal.length) {
+
+      const $clickedElem = $(e.target);
+      const isTitleClicked = $clickedElem.is('.card-reveal .card-title');
+      const isTitleIconClicked = $clickedElem.is('.card-reveal .card-title i');
+      const isActivatorClicked = $clickedElem.is('.card .activator');
+      const isActivatorIconClicked = $clickedElem.is('.card .activator i');
+
+      if (isTitleClicked || isTitleIconClicked) {
+        takeRevealDown($reveal);
+      } else if (isActivatorClicked || isActivatorIconClicked) {
+        takeRevealUp($reveal);
+      }
+    }
+  });
+
+  const takeRevealUp = (revealElem) => {
+
+    revealElem.css({
+      display: 'block'
+    }).velocity({
+      translateY: '-100%'
+    }, {
+      duration: 300,
+      queue: false,
+      easing: 'easeInOutQuad'
+    });
+  };
+
+  const takeRevealDown = (revealElem) => {
+
+    revealElem.velocity({
+      translateY: 0
+    }, {
+      duration: 225,
+      queue: false,
+      easing: 'easeInOutQuad',
+      complete: function complete() {
+        revealElem.css({
+          display: 'none'
+        });
+      }
+    });
+  };
+
+  $('.rotate-btn').on('click', function () {
+
+    $(this).closest('.card').toggleClass('flipped');
+
+  });
+
+  $(window).on('load', function () {
+
+    const $rotatingCards = $('.card-rotating');
+    $rotatingCards.each(function () {
+      const $this = $(this);
+
+      const $cardWrapper = $this.parent();
+      const $front = $this.find('.front');
+      const $back = $this.find('.back');
+      const $frontHeight = $this.find('.front').outerHeight();
+      const $backHeight = $this.find('.back').outerHeight();
+
+      if ($frontHeight > $backHeight) $($cardWrapper, $back).height($frontHeight);
+      else if ($frontHeight < $backHeight) $($cardWrapper, $front).height($backHeight);
+      else $($cardWrapper).height($backHeight);
+    });
+
+  });
+
+  $('.card-share > a').on('click', function (e) {
+
+    e.preventDefault();
+
+    $(this)
+      .toggleClass('share-expanded')
+      .parent()
+      .find('div')
+      .toggleClass('social-reveal-active');
+  });
+
+  $('.map-card').on('click', function () {
+    $(this).find('.card-body').toggleClass('closed');
+  });
+
+});
