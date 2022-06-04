64e7a80eadd01cb6b7a2f6b8c74c473f5b61a3ee
Christopher David Alberto Roque <christopherdavidalbertoroque@gmail.com>
2022-06-03 23:52:02 -0400

    Composer

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('database','session', 'parser','user_agent');
+$autoload['libraries'] = array('database','session', 'parser','user_agent','email');
 
 /*
 | -------------------------------------------------------------------
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('url');
+$autoload['helper'] = array('url','email');
 
 /*
 | -------------------------------------------------------------------
