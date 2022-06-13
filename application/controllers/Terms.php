e30d8617f3b176a7c198c11eb0bcf00ca2045e80
Christopher David Alberto Roque <christopherdavidalbertoroque@gmail.com>
2022-06-13 03:09:08 -0500

    Error Legals

--- application/controllers/Terms.php
+++ application/controllers/Terms.php
@@ -10,6 +10,7 @@ class Terms extends CI_Controller {
 		$this->load->model('Apiserverstats');
 		$this->load->model('Apiusers');
 		$this->load->model('Apiplayers');
+        $this->load->model('Langs');
     }
 	
 	public function index()
@@ -20,10 +21,81 @@ class Terms extends CI_Controller {
         $base_url = base_url();
         header("Location: $base_url/mant");
         }else{
-            $this->load->view('header');
-            $this->load->view('navbar');
-            $this->load->view('terms');
-            $this->load->view('footer');
+            $lang = $this->Langs->lang();
+			switch($this->session->userdata('lang')){
+				case "es":
+					$this->parser->parse('header', $lang[0]); 
+					$this->parser->parse('navbar', $lang[0]); 
+					$this->parser->parse('terms', $lang[0]); 
+					$this->parser->parse('footer', $lang[0]); 
+					break;
+				case "en":
+					$this->parser->parse('header', $lang[1]); 
+					$this->parser->parse('navbar', $lang[1]); 
+					$this->parser->parse('terms', $lang[1]); 
+					$this->parser->parse('footer', $lang[1]); 
+					break;
+				case "tr":
+					$this->parser->parse('header', $lang[2]); 
+					$this->parser->parse('navbar', $lang[2]); 
+					$this->parser->parse('terms', $lang[2]); 
+					$this->parser->parse('footer', $lang[2]); 
+					break;
+				case "jp":
+					$this->parser->parse('header', $lang[3]); 
+					$this->parser->parse('navbar', $lang[3]); 
+					$this->parser->parse('terms', $lang[3]); 
+					$this->parser->parse('footer', $lang[3]); 
+					break;
+					case "de":
+						$this->parser->parse('header', $lang[4]); 
+						$this->parser->parse('navbar', $lang[4]); 
+						$this->parser->parse('terms', $lang[4]); 
+						$this->parser->parse('footer', $lang[4]); 
+						break;	
+					case "ru":
+						$this->parser->parse('header', $lang[5]); 
+						$this->parser->parse('navbar', $lang[5]); 
+						$this->parser->parse('terms', $lang[5]); 
+						$this->parser->parse('footer', $lang[5]); 
+						break;
+					case "zh":
+						$this->parser->parse('header', $lang[6]); 
+						$this->parser->parse('navbar', $lang[6]); 
+						$this->parser->parse('terms', $lang[6]); 
+						$this->parser->parse('footer', $lang[6]); 
+						break;	
+					case "fr":
+						$this->parser->parse('header', $lang[7]); 
+						$this->parser->parse('navbar', $lang[7]); 
+						$this->parser->parse('terms', $lang[7]); 
+						$this->parser->parse('footer', $lang[7]); 
+						break;	
+					case "pt":
+						$this->parser->parse('header', $lang[8]); 
+						$this->parser->parse('navbar', $lang[8]); 
+						$this->parser->parse('terms', $lang[8]); 
+						$this->parser->parse('footer', $lang[8]); 
+						break;
+					case "hi":
+						$this->parser->parse('header', $lang[9]); 
+						$this->parser->parse('navbar', $lang[9]); 
+						$this->parser->parse('terms', $lang[9]); 
+						$this->parser->parse('footer', $lang[9]); 
+						break;	
+					case "ar":
+						$this->parser->parse('header', $lang[10]); 
+						$this->parser->parse('navbar', $lang[10]); 
+						$this->parser->parse('terms', $lang[10]); 
+						$this->parser->parse('footer', $lang[10]); 
+						break;						
+				default:
+					$this->parser->parse('header', $lang[0]); 
+					$this->parser->parse('navbar', $lang[0]); 
+					$this->parser->parse('terms', $lang[0]); 
+					$this->parser->parse('footer', $lang[0]); 
+					break;
+                    }
         } 
        
 	}
